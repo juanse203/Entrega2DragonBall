29b50450357f42fbf355485488bdfea0d09313c7
juanse203 <jsrojas203@gmail.com>
2025-04-12 15:49:52 -0500

    odio react con todo mi ser

--- src/Pages/FilterPage/FilterPage.jsx
+++ src/Pages/FilterPage/FilterPage.jsx
@@ -3,19 +3,20 @@ import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import CardCharacter from '../../Components/CardCharacter/CardCharacter'
 
+
 const FilterPage = () => {
     const { gender } = useParams();
     const [arrayCaracteres, setArrayCaracteres] = useState([]);
 
     const fetchCharacters = async () => {
-        await fetch(`https://dragonball-api.com/api/characters`)
-            .then((response) => response.json())
-            .then((data) => {
-                const filtrados = data.items.filter((items) => items.gender === gender);
-                setArrayCaracteres(filtrados);
-                console.log(filtrados)
-            })
-            .catch((error) => console.error('Error:', error));
+        try {
+            const response = await fetch(`https://dragonball-api.com/api/characters?gender=${gender}`);
+            const data = await response.json();
+            setArrayCaracteres(data);
+            console.log(data);
+        } catch (error) {
+            console.error('Error:', error);
+        }
     };
 
     useEffect(() => {
@@ -24,9 +25,9 @@ const FilterPage = () => {
 
     return (
         <main>
-            {arrayCaracteres.map((items) => (
-                <Link to={`/details/${items.id}`} key={items.id}>
-                    <CardCharacter key={items.id} items={items} />
+            {arrayCaracteres.map((item) => (
+                <Link to={`/details/${item.id}`} key={item.id}>
+                    <CardCharacter items={item} />
                 </Link>
             ))}
         </main>
